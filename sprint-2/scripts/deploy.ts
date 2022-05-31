1ab2df07584a84a393eae1974edc303fc4546b09
GeraldRyan <gerald.ryan40@yahoo.com>
2022-05-30 20:25:29 -0500

    layout

---
+++ sprint-2/scripts/deploy.ts
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const ConnectFour = await ethers.getContractFactory("ConnectFour");
+  const c4 = await ConnectFour.deploy(0, ethers.utils.parseUnits("1.0"));
+
+  await c4.deployed();
+
+  console.log("C4 deployed to:", c4.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
