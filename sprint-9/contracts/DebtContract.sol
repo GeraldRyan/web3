ab16a5319a83c080bf3e352d5311378749a168b6
GeraldRyan <gerald.ryan40@protonmail.com>
2022-07-28 23:49:28 -0500

    sprint 9 contract started

--- sprint-9/contracts/DebtContract.sol
+++ sprint-9/contracts/DebtContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// import {DAI} from './DAI.sol';
+import {DAI} from './DAI.sol';
 
 contract BasicLoan{
     struct Terms{
@@ -25,6 +25,38 @@ contract BasicLoan{
         daiAddress = _daiAddress;
         lender = payable(msg.sender);
         state = LoanState.Created;
+    }
+
+    modifier onlyInState(LoanState expectedState){
+        require(state == expectedState, "Not allowed in this state");
+        _;
+    }
+
+    function fundLoan() public onlyInState(LoanState.Created){
+        state = LoanState.Funded;
+        DAI(daiAddress).transferFrom(msg.sender, address(this), terms.loanDaiAmount);
+    }
+
+    function takeALoanAndAcceptLoanTerms()
+    public 
+    payable 
+    onlyInState(LoanState.Funded){
+        require(msg.value == terms.ethCollateralAmount, "Invalid Collateral amount");
+        borrower = payable(msg.sender);
+        state = LoanState.Taken;
+        DAI(daiAddress).transfer(borrower, terms.loanDaiAmount);
+    }
+
+    function repay() public onlyInState(LoanState.Taken){
+        require(msg.sender == borrower, "Only the borrower can repay the loan");
+        DAI(daiAddress).transferFrom(borrower, lender, terms.loanDaiAmount + terms.feeDaiAmount);
+        selfdestruct(borrower);
+    }
+
+    function liquidate() public onlyInState(LoanState.Taken){
+        require(msg.sender == lender, "Only the lender can liquidate the loan");
+        require(block.timestamp >= terms.repayByTimestamp, "Cannot liquidate before the loan is due");
+        selfdestruct(lender);
     }
 
 
